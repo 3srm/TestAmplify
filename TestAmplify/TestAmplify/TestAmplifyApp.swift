0400ce8c6ac99e5d1b5e5ef47545b7887ae5b708
Abdulaziz Fallatah <90557938+3srm@users.noreply.github.com>
2024-12-27 10:57:48 +0300

    Add project

---
+++ TestAmplify/TestAmplify/TestAmplifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAmplifyApp.swift
+//  TestAmplify
+//
+//  Created by Abdulaziz Fallatah on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAmplifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
